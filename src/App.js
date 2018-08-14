188fcb6813566bfacae80f342fdb384b075a241f
njihiamark <njihiamark@gmail.com>
2018-08-14 08:01:57 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import SkillsHeader from './components/SkillsHeader';
+
+function App() {
+  return (
+    <SkillsHeader />
+  );
+}
+
+export default App;
---
+++ src/components/SkillsHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function SkillsHeader() {
+  return (
+    <div>
+      <h3 className="lead">Select your skills</h3>
+      <p className="lead">Please choose at least 5 most important skills you have</p>
+    </div>
+
+  );
+}
+
+export default SkillsHeader;
