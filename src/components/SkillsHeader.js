91321245a509f2bbb921e90086f1ddc641566684
njihiamark <njihiamark@gmail.com>
2018-08-14 13:43:50 +0300

    made the title card to satisfaction

--- src/components/SkillsHeader.js
+++ src/components/SkillsHeader.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 
+const styles = {
+  card: {
+    minWidth: 275,
+  }
+};
 
-function SkillsHeader() {
+function SkillsHeader(props) {
+  const { classes } = props;
   return (
     <div>
-      <h3 className="lead">Select your skills</h3>
-      <p className="lead">Please choose at least 5 most important skills you have</p>
+      <Grid item md={4}>
+        <Card className={classes.card}>
+          <CardContent>
+            <h3 className="lead">Select your skills</h3>
+            <p className="lead">Please choose at least 5 most important skills you have</p>
+          </CardContent>
+        </Card>
+      </Grid>
     </div>
 
   );
 }
 
-export default SkillsHeader;
+SkillsHeader.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SkillsHeader);
